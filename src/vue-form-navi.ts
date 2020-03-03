28b1dde9f84955f152f9eaf4de13c39757b3130f
Yoyo Young <yoyoys@livemail.tw>
2020-03-03 14:36:59 +0800

    feat: remove jquery dependency

--- src/vue-form-navi.ts
+++ src/vue-form-navi.ts
@@ -1,84 +1,82 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { DirectiveOptions } from 'vue';
-import $ from 'jquery';
 
-const nodeList = 'button, [href], input, [tabindex]:not([tabindex="-1"])';
+const nodeList = 'button, [href], input, textarea,, [tabindex]:not([tabindex="-1"])';
+// eslint-disable-next-line prettier/prettier
+const targetList = 'button, [href], input, [tabindex]:not(textarea):not(select):not([tabindex="-1"])';
 
 const handler: (e: Event) => void = e => {
   if (!(e instanceof KeyboardEvent) || !e.target) return;
+  const eventTarget = e.target as HTMLElement;
 
   switch (e.which) {
     case 39:
       if (!(e.target instanceof HTMLInputElement)) {
         e.preventDefault();
-        $(e.target)
-          .closest('td')
-          .next()
-          .find(nodeList)
+        const next = eventTarget.closest('td')?.nextSibling as HTMLElement | null;
+        if (!next) break;
 
-          .focus();
+        const target: HTMLElement | null = next.querySelector(nodeList);
+        if (!target) break;
+        target.focus();
         break;
       }
 
       if (e.target.selectionStart === e.target.value.length) {
         e.preventDefault();
-        $(e.target)
-          .closest('td')
-          .next()
-          .find(nodeList)
+        const next = eventTarget.closest('td')?.nextSibling as HTMLElement | null;
+        if (!next) break;
 
-          .focus();
+        const target: HTMLElement | null = next.querySelector(nodeList);
+        if (!target) break;
+        target.focus();
       }
       break;
     case 37:
       if (!(e.target instanceof HTMLInputElement)) {
         e.preventDefault();
-        $(e.target)
-          .closest('td')
-          .prev()
-          .find(nodeList)
-          .focus();
+        const next = eventTarget.closest('td')?.previousSibling as HTMLElement | null;
+        if (!next) break;
+
+        const target: HTMLElement | null = next.querySelector(nodeList);
+        if (!target) break;
+        target.focus();
         break;
       }
 
       if (e.target.selectionStart === 0) {
         e.preventDefault();
-        $(e.target)
-          .closest('td')
-          .prev()
-          .find(nodeList)
+        const prev = eventTarget.closest('td')?.previousSibling as HTMLElement | null;
+        if (!prev) break;
 
-          .focus();
+        const target: HTMLElement | null = prev.querySelector(nodeList);
+        if (!target) break;
+        target.focus();
       }
 
       break;
-    case 40:
+    case 40: {
       e.preventDefault();
-      $(e.target)
-        .closest('tr')
-        .next()
-        .children()
-        .eq(
-          $(e.target)
-            .closest('td')
-            .index(),
-        )
-        .find(nodeList)
-        .focus();
+      const next = eventTarget.closest('tr')?.nextSibling as HTMLElement | null;
+      const index = eventTarget.closest('td')?.cellIndex;
+      if (!index || !next) break;
+      const nextTd = next.childNodes[index] as HTMLElement;
+      const target: HTMLElement | null = nextTd.querySelector(nodeList);
+      if (!target) break;
+      target.focus();
       break;
-    case 38:
+    }
+    case 38: {
       e.preventDefault();
-      $(e.target)
-        .closest('tr')
-        .prev()
-        .children()
-        .eq(
-          $(e.target)
-            .closest('td')
-            .index(),
-        )
-        .find(nodeList)
-        .focus();
+      const prev = eventTarget.closest('tr')?.previousSibling as HTMLElement | null;
+      const index = eventTarget.closest('td')?.cellIndex;
+      if (!index || !prev) break;
+      const prevTd = prev.childNodes[index] as HTMLElement;
+      const target: HTMLElement | null = prevTd.querySelector(nodeList);
+      if (!target) break;
+      target.focus();
       break;
+    }
     default:
       break;
   }
@@ -86,11 +84,11 @@ const handler: (e: Event) => void = e => {
 
 const formNavi: DirectiveOptions = {
   bind(el) {
-    const focusable: NodeList = el.querySelectorAll(nodeList);
+    const focusable: NodeList = el.querySelectorAll(targetList);
     focusable.forEach(elem => elem.addEventListener('keydown', handler));
   },
   unbind(el) {
-    const focusable: NodeList = el.querySelectorAll(nodeList);
+    const focusable: NodeList = el.querySelectorAll(targetList);
     focusable.forEach(elem => elem.removeEventListener('keydown', handler));
   },
 };
