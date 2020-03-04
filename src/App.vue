501123b1ae266dd0b685f0ffe1be053955d1d57a
Yoyo Young <yoyoys@livemail.tw>
2020-03-04 23:24:22 +0800

    feat: refactor for event listener

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
           <th>1</th>
           <td><input type="text" style="width:50px" /></td>
           <td><input type="text" style="width:50px" /></td>
-          <td><input type="text" style="width:50px" /></td>
+          <td><button>Test</button></td>
         </tr>
         <tr>
           <th>2</th>
@@ -25,21 +25,37 @@
         <tr>
           <th>3</th>
           <td><input type="text" style="width:50px" /></td>
-          <td><button></button></td>
+          <td><textarea></textarea></td>
           <td><input type="text" style="width:50px" /></td>
         </tr>
+        <template v-if="hasTable">
+          <tr>
+            <th>4</th>
+            <td><input type="text" style="width:50px" /></td>
+            <td><button></button></td>
+            <td><input type="text" style="width:50px" /></td>
+          </tr>
+        </template>
       </tbody>
     </table>
+    <button @click="toggle">toggle</button>
   </div>
 </template>
 
 <script lang="ts">
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { Component, Vue } from 'vue-property-decorator';
-import formNavi from './vue-form-navi';
+import formNavi from './vue-form-navi/index';
 @Component({
   directives: { formNavi },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  protected hasTable = false;
+
+  protected toggle() {
+    this.hasTable = !this.hasTable;
+  }
+}
 </script>
 
 <style></style>
--- src/vue-form-navi/index.ts
+++ src/vue-form-navi/index.ts
@@ -1,13 +1,18 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { DirectiveOptions } from 'vue';
 
-const nodeList = 'button, [href], input, textarea,, [tabindex]:not([tabindex="-1"])';
-// eslint-disable-next-line prettier/prettier
-const targetList = 'button, [href], input, [tabindex]:not(textarea):not(select):not([tabindex="-1"])';
+const nodeList = 'button, [href], input, textarea, select, [tabindex]:not([tabindex="-1"])';
 
 const handler: (e: Event) => void = e => {
   if (!(e instanceof KeyboardEvent) || !e.target) return;
   const eventTarget = e.target as HTMLElement;
+  const blackList = ['select', 'textarea'];
+
+  if (blackList.includes(eventTarget.tagName.toLowerCase())) return;
+  const doFocus = (target: HTMLElement | null) => {
+    if (!target) return;
+    target.focus();
+  };
 
   switch (e.which) {
     case 39:
@@ -17,8 +22,8 @@ const handler: (e: Event) => void = e => {
         if (!next) break;
 
         const target: HTMLElement | null = next.querySelector(nodeList);
-        if (!target) break;
-        target.focus();
+        doFocus(target);
+
         break;
       }
 
@@ -28,8 +33,7 @@ const handler: (e: Event) => void = e => {
         if (!next) break;
 
         const target: HTMLElement | null = next.querySelector(nodeList);
-        if (!target) break;
-        target.focus();
+        doFocus(target);
       }
       break;
     case 37:
@@ -39,8 +43,8 @@ const handler: (e: Event) => void = e => {
         if (!next) break;
 
         const target: HTMLElement | null = next.querySelector(nodeList);
-        if (!target) break;
-        target.focus();
+        doFocus(target);
+
         break;
       }
 
@@ -50,8 +54,7 @@ const handler: (e: Event) => void = e => {
         if (!prev) break;
 
         const target: HTMLElement | null = prev.querySelector(nodeList);
-        if (!target) break;
-        target.focus();
+        doFocus(target);
       }
 
       break;
@@ -60,21 +63,24 @@ const handler: (e: Event) => void = e => {
       const next = eventTarget.closest('tr')?.nextSibling as HTMLElement | null;
       const index = eventTarget.closest('td')?.cellIndex;
       if (!index || !next) break;
-      const nextTd = next.childNodes[index] as HTMLElement;
+      const nextTd = next.childNodes[index] as HTMLElement | null;
+      if (!nextTd) break;
       const target: HTMLElement | null = nextTd.querySelector(nodeList);
-      if (!target) break;
-      target.focus();
+      doFocus(target);
+
       break;
     }
     case 38: {
       e.preventDefault();
       const prev = eventTarget.closest('tr')?.previousSibling as HTMLElement | null;
       const index = eventTarget.closest('td')?.cellIndex;
+
       if (!index || !prev) break;
-      const prevTd = prev.childNodes[index] as HTMLElement;
+      const prevTd = prev.childNodes[index] as HTMLElement | null;
+      if (!prevTd) break;
       const target: HTMLElement | null = prevTd.querySelector(nodeList);
-      if (!target) break;
-      target.focus();
+      doFocus(target);
+
       break;
     }
     default:
@@ -84,12 +90,10 @@ const handler: (e: Event) => void = e => {
 
 const formNavi: DirectiveOptions = {
   bind(el) {
-    const focusable: NodeList = el.querySelectorAll(targetList);
-    focusable.forEach(elem => elem.addEventListener('keydown', handler));
+    el.addEventListener('keydown', handler);
   },
   unbind(el) {
-    const focusable: NodeList = el.querySelectorAll(targetList);
-    focusable.forEach(elem => elem.removeEventListener('keydown', handler));
+    el.removeEventListener('keydown', handler);
   },
 };
 
